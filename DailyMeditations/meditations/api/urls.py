eebbb382088bbead8a2086ef678a8ec14d2cf654
d2fong <d2fong@uwaterloo.ca>
2016-10-03 19:04:05 -0400

    Implements routes for the meditation views

--- DailyMeditations/meditations/api/urls.py
+++ DailyMeditations/meditations/api/urls.py
@@ -8,4 +8,10 @@ urlpatterns = [
     url(r'^authors/(?P<pk>\d+)/$', 
         views.AuthorDetailView.as_view(),
         name='subject_detail'),
+    url(r'^meditations/$',
+        views.MeditationListView.as_view(),
+        name='meditation_list'),
+    url(r'^meditations/(?P<pk>\d+)/$', 
+        views.MeditationDetailView.as_view(),
+        name='meditation_detail'),
 ]
