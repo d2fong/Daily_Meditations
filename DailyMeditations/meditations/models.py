6b41dc3be47832b9b95d8e60b7ef6d08ade13f75
d2fong <d2fong@uwaterloo.ca>
2016-10-03 13:30:48 -0400

    Adds models for author and meditation

--- DailyMeditations/meditations/models.py
+++ DailyMeditations/meditations/models.py
@@ -1,3 +1,26 @@
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
+
+
+class Meditation(models.Model):
+	STOICISM = 0
+
+	CATEGORY = (
+		(STOICISM, 'stoicism'),
+	)
+
+	author = models.ForeignKey('Author')
+	category = models.IntegerField(choices=CATEGORY)
+	content = models.TextField()
+	created_date = models.DateTimeField(
+		default = timezone.now)
+	published_date = models.DateTimeField(
+		blank=True, null=True)
+
+
+class Author(models.Model):
+	first_name = models.CharField(max_length=30)
+	last_name = models.CharField(max_length=30)
+
