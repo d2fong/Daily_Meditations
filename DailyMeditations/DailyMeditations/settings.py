065e3e559d99c48c3603ca7344e3541d919affab
d2fong <d2fong@uwaterloo.ca>
2016-10-04 16:37:11 -0400

    Removes data base settings into the secrets file, switches to mysql

--- DailyMeditations/DailyMeditations/settings.py
+++ DailyMeditations/DailyMeditations/settings.py
@@ -13,7 +13,7 @@ https://docs.djangoproject.com/en/1.8/ref/settings/
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
 
-from secrets import *
+from .secrets import *
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -75,17 +75,6 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'DailyMeditations.wsgi.application'
-
-
-# Database
-# https://docs.djangoproject.com/en/1.8/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
 
 
 # Internationalization
