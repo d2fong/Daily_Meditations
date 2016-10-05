608c4647103075259b8976bcea20fd220b547bef
d2fong <d2fong@uwaterloo.ca>
2016-10-05 18:30:02 -0400

    herokuapp settings

--- DailyMeditations/settings.py
+++ DailyMeditations/settings.py
@@ -149,7 +149,7 @@ SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
 DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['.herokuapp.com']
 
 SECRET_KEY = 'sdf*21)k*u1#s#%&kq%s)*^nz+y8()o9hny&_fi8&@*ay1yr!qfe43$@G'
 
